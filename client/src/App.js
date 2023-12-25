0395833de08e3f3410c2a4f949736818cd86b4c3
fahmi <fahmi.sifi.contact@gmail.com>
2023-12-25 17:52:14 +0400

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
-    <div>
-      app js
+    <div className='layout'>
+       <Header/>
+       <main>main</main>
+       <footer>footer</footer>
     </div>
   );
 }
