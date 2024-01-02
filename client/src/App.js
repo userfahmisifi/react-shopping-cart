0d66ef44dd05731aa9a411fbfde60754ef8a56a8
fahmi <fahmi.sifi.contact@gmail.com>
2024-01-02 19:35:47 +0400

    filter comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
 
-import React , { useState } from 'react'
+import React, { useState } from 'react'
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 import data from './data.json'
+import Filter from './components/Filter/Filter';
 
 
 function App() {
- const [products,setProducts]=useState(data)
+  const [products, setProducts] = useState(data)
 
   return (
     <div className='layout'>
       <Header />
       <main>
         <div className="products">
-          <Products products={products}/>
-          <div className="products-filter">filter</div>
+          <Products products={products} data={data} />
+          <Filter data={data} setProducts={setProducts} />
         </div>
       </main>
       <Footer />
