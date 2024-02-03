edd6b23bcc210420c733ca6767ca59da8306d880
fahmi <fahmi.sifi.contact@gmail.com>
2024-02-03 23:07:02 +0400

    change in db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const uri = process.env.CONN_STR || "mongodb://localhost:27017/shopping-cart";
-mongoose.set("strictQuery", false);
 
 module.exports = function () {
   mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(uri)
     .then((conn) => console.log("connection to database success"))
     .catch((error) => console.log("connection to database fail"));
 };
